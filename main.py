0b83a4ddcb7309116bbc014abf3099b1d252a65b
Idan Itzhaky <idan.itzhaky@overwolf.com>
2025-02-07 21:27:59 +0200

    ligihtpin

--- main.py
+++ main.py
@@ -3,20 +3,26 @@ import time
 import RPi.GPIO as GPIO
 
 # Pin definitions
-pin1 = 2
-pin2 = 3
+sensor1Pin = 2
+sensor2Pin = 3
+lightPin = 4
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin1, GPIO.IN)
-GPIO.setup(pin2, GPIO.IN)
+GPIO.setup(sensor1Pin, GPIO.IN)
+GPIO.setup(sensor2Pin, GPIO.IN)
+GPIO.setup(lightPin, GPIO.OUT)
 
 try:
   while True:
-    if GPIO.input(pin1) == GPIO.HIGH:
+    if GPIO.input(sensor1Pin) == GPIO.HIGH:
       print("Pin 1 is HIGH")
-    if GPIO.input(pin2) == GPIO.HIGH:
+    if GPIO.input(sensor2Pin) == GPIO.HIGH:
       print("Pin 2 is HIGH")
+    if GPIO.input(sensor1Pin) == GPIO.HIGH and GPIO.input(sensor2Pin) == GPIO.HIGH:
+      GPIO.output(lightPin, GPIO.HIGH)
+    else:
+      GPIO.output(lightPin, GPIO.LOW)
     time.sleep(0.1)
 except KeyboardInterrupt:
   pass
